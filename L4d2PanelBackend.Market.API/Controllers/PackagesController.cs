7860f0c6f2c06f41832d4e8f7ddd37fe863c1de5
deximy <deximy@gmail.com>
2023-01-06 05:57:23 +0800

    Update project Market

---
+++ L4d2PanelBackend.Market.API/Controllers/PackagesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L4d2PanelBackend.Market.API.Controllers
+{
+    [Route("packages")]
+    [ApiController]
+    public class PackagesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPackagesList()
+        {
+            return Ok();
+        }
+    }
+}
