aac208c8e8e177d87fe470f610c3ada25de214e7
deximy <deximy@gmail.com>
2023-03-23 02:11:59 +0800

    refactor: refactor with functions

--- linux_run_this.sh
+++ linux_run_this.sh
@@ -1,55 +1,55 @@
 #!/bin/sh
 set -e
 
+if_docker_exist() {
+  if !command -v docker >/dev/null 2>&1; then
+    return 1
+  fi
+  return 0
+}
 
-
-echo "开始安装……"
-
-
-
-echo "正在检测是否已安装基础组件: docker"
-
-if ! command -v docker >/dev/null 2>&1 ; then
-  echo "未安装基础组件 docker，正在安装……"
+install_docker() {
   curl -fsSL https://get.docker.com -o get-docker.sh
   sudo sh get-docker.sh
   rm get-docker.sh
-  echo "基础组件 docker 安装完成。"
-fi
+}
 
-echo "当前 docker 版本为：$(docker --version | awk '{print $3}' | tr -d ',') docker-compose 版本为：$(docker compose version | awk '{print $4}' | tr -d ',')"
+print_docker_info() {
+  echo "当前 docker 版本为：$(docker --version | awk '{print $3}' | tr -d ',') docker-compose 版本为：$(docker compose version | awk '{print $4}' | tr -d ',')"
+}
 
-
-
-echo "正在生成配置文件……"
-
-read -p "请输入安装目录（默认为当前目录）: " install_dir
-if [ -z "$install_dir" ]; then
-  install_dir="$(pwd)"
-fi
-
-if [ ! -d "$install_dir" ]; then
-  read -p "$install_dir 不存在，是否创建？[Y/n] " create_dir
-  if [ "$create_dir" == "Y" ] || [ "$create_dir" == "y" ] || [ -z "$create_dir" ]; then
-    mkdir -p "$install_dir"
-  else
-    echo "安装目录不存在，退出安装程序。"
-    exit 1
+get_install_dir() {
+  read install_dir
+  if [ -z "$install_dir" ]; then
+    install_dir="$(pwd)"
   fi
-else
-  if [ "$(ls -A "$install_dir")" ]; then
-    read -p "$install_dir 不为空，是否继续？[Y/n] " continue_install
-    if [ "$continue_install" != "Y" ] && [ "$continue_install" != "y" ] && [ -n "$continue_install" ]; then
-      echo "退出安装程序。"
+  echo "$install_dir"
+}
+
+prepare_install_dir() {
+  if [ ! -d "$1" ]; then
+    read -p "$1 不存在，是否创建？[Y/n] " create_dir
+    if [ "$create_dir" = "Y" ] || [ "$create_dir" = "y" ] || [ -z "$create_dir" ]; then
+      mkdir -p "$1"
+    else
+      echo "安装目录不存在，退出安装程序。"
       exit 1
     fi
+  else
+    if [ "$(ls -A "$1")" ]; then
+      read -p "$1 不为空，是否继续？[Y/n] " continue_install
+      if [ "$continue_install" != "Y" ] && [ "$continue_install" != "y" ] && [ -n "$continue_install" ]; then
+        echo "退出安装程序。"
+        exit 1
+      fi
+    fi
   fi
-fi
 
-echo "安装目录为：$install_dir"
-mkdir -p "$install_dir/addons" "$install_dir/cfg" "$install_dir/models" "$install_dir/sound" "$install_dir/scripts"
+  mkdir -p "$1/addons" "$1/cfg" "$1/models" "$1/sound" "$1/scripts"
+}
 
-echo "services:
+generate_config_file() {
+  echo "services:
   l4d2:
     image: deximy/l4d2panel
     container_name: l4d2
@@ -63,35 +63,65 @@ echo "services:
       - models:/l4d2/left4dead2/models
       - sound:/l4d2/left4dead2/sound
       - scripts:/l4d2/left4dead2/scripts
-volumes:
-  addons:
-    driver: local
-    driver_opts:
-      type: none
-      device: $install_dir/addons
-      o: bind
-  cfg:
-    driver: local
-    driver_opts:
-      type: none
-      device: $install_dir/cfg
-      o: bind
-  models:
-    driver: local
-    driver_opts:
-      type: none
-      device: $install_dir/models
-      o: bind
-  sound:
-    driver: local
-    driver_opts:
-      type: none
-      device: $install_dir/sound
-      o: bind
-  scripts:
-    driver: local
-    driver_opts:
-      type: none
-      device: $install_dir/scripts
-      o: bind
-" > $install_dir/docker-compose.yml
+  volumes:
+    addons:
+      driver: local
+      driver_opts:
+        type: none
+        device: $1/addons
+        o: bind
+    cfg:
+      driver: local
+      driver_opts:
+        type: none
+        device: $1/cfg
+        o: bind
+    models:
+      driver: local
+      driver_opts:
+        type: none
+        device: $1/models
+        o: bind
+    sound:
+      driver: local
+      driver_opts:
+        type: none
+        device: $1/sound
+        o: bind
+    scripts:
+      driver: local
+      driver_opts:
+        type: none
+        device: $1/scripts
+        o: bind
+  " > $1/docker-compose.yml
+
+}
+
+install_l4d2panel() {
+  echo "正在检测是否已安装基础组件: docker"
+  if ! if_docker_exist; then
+    echo "未安装基础组件 docker，正在安装……"
+    install_docker
+    echo "基础组件 docker 安装完成。"
+  else
+    echo "基础组件 docker 已安装。"
+  fi
+  
+  print_docker_info
+  
+  echo -n "请输入安装目录(默认为当前目录): "
+  install_dir=$(get_install_dir)
+  echo "安装目录为：$install_dir"
+
+  prepare_install_dir $install_dir
+
+  echo "正在生成配置文件……"
+  generate_config_file $install_dir
+  echo "配置文件生成完成。"
+
+}
+
+echo "开始安装……"
+install_l4d2panel
+echo "安装完成。"
